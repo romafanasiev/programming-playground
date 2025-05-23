602ea4518af59b05587fbb339e2afbca31152f44
romafanasiev <romafanasiev123@gmail.com>
2025-05-23 08:11:59 +0300

    feat: add abstract factory example

--- gof/factory-method/example.ts
+++ gof/factory-method/example.ts
@@ -75,7 +75,7 @@ class ConcreteProduct2 implements Product {
  * its base interface. As long as the client keeps working with the creator via
  * the base interface, you can pass it any creator's subclass.
  */
-function clientCode(creator: Creator) {
+function clientCodeFactoryMethod(creator: Creator) {
   // ...
   console.log(
     "Client: I'm not aware of the creator's class, but it still works.",
@@ -89,8 +89,8 @@ function clientCode(creator: Creator) {
  * environment.
  */
 console.log('App: Launched with the ConcreteCreator1.');
-clientCode(new ConcreteCreator1());
+clientCodeFactoryMethod(new ConcreteCreator1());
 console.log('');
 
 console.log('App: Launched with the ConcreteCreator2.');
-clientCode(new ConcreteCreator2());
+clientCodeFactoryMethod(new ConcreteCreator2());
